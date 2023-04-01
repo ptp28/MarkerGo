f5cfbe62c3175a22694b1ca74ed795bdee0373bf
ptp28 <parthparikh28@gmail.com>
2023-03-31 20:46:30 -0400

    Added onClick listeners and text view setters

--- MarkerGo/app/src/main/java/edu/northeastern/markergo/LocationDetailsActivity.java
+++ MarkerGo/app/src/main/java/edu/northeastern/markergo/LocationDetailsActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,6 +19,9 @@ import java.util.List;
 public class LocationDetailsActivity extends AppCompatActivity {
 
     private RecyclerView recyclerViewImages;
+    private TextView descriptionTextView;
+    private Button getDirectionsButton;
+    private Button checkInButton;
     RecyclerView.LayoutManager imageGridLayoutManager;
     ImageRecyclerViewAdapter recyclerViewAdapter;
     List<Integer> imageList;
@@ -23,6 +31,9 @@ public class LocationDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_location_details);
 
         recyclerViewImages = findViewById(R.id.recyclerViewImages);
+        descriptionTextView = (TextView) findViewById(R.id.textViewDescription);
+        getDirectionsButton = (Button) findViewById(R.id.buttonGetDirections);
+        checkInButton = (Button) findViewById(R.id.buttonCheckIn);
         imageGridLayoutManager = new GridLayoutManager(this, 3);
         recyclerViewImages.setLayoutManager(imageGridLayoutManager);
 
@@ -41,5 +52,26 @@ public class LocationDetailsActivity extends AppCompatActivity {
 
         recyclerViewImages.setAdapter(recyclerViewAdapter);
         recyclerViewImages.setHasFixedSize(true);
+
+        checkInButton.setOnClickListener(checkInButtonListener);
+        getDirectionsButton.setOnClickListener(getDirectionsListener);
     }
+
+    private void setDescription(String description) {
+        descriptionTextView.setText(description);
+    }
+
+    View.OnClickListener checkInButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(LocationDetailsActivity.this, "Check In", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    View.OnClickListener getDirectionsListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(LocationDetailsActivity.this, "Get Directions", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
