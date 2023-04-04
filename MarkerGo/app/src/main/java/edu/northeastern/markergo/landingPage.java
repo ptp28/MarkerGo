aeed4073a57a2e10ee23fae9cca13706deb959d4
GrishmaAlshi <grishma.alshi@gmail.com>
2023-04-04 14:32:25 -0400

    Added the menu on landing page

--- MarkerGo/app/src/main/java/edu/northeastern/markergo/landingPage.java
+++ MarkerGo/app/src/main/java/edu/northeastern/markergo/landingPage.java
@@ -3,6 +3,7 @@ package edu.northeastern.markergo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class landingPage extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class landingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
     }
 }
