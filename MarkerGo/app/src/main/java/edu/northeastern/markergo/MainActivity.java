2102d8f33a050955da67b141b51c89a9bf60c0dd
GrishmaAlshi <grishma.alshi@gmail.com>
2023-04-04 13:58:08 -0400

    added google maps api key and added map to the maps activity page

--- MarkerGo/app/src/main/java/edu/northeastern/markergo/MainActivity.java
+++ MarkerGo/app/src/main/java/edu/northeastern/markergo/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mapsButton = findViewById(R.id.buttonMapsActivity);
+        if (mapsButton != null) {
+            mapsButton.setOnClickListener((it -> {
+                Intent intent=new Intent(MainActivity.this,landingPage.class);
+                startActivity(intent);
+            }));
+        }
     }
 
     public void openLocationDetailsActivity(View view) {
         startActivity(new Intent(getApplicationContext(), LocationDetailsActivity.class));
     }
+
+
 }
