862366b3f5067b224f000dbbb96bdafb32add56e
ptp28 <parthparikh28@gmail.com>
2023-04-07 19:48:01 -0400

    Open maps activity

--- MarkerGo/app/src/main/java/edu/northeastern/markergo/MainActivity.java
+++ MarkerGo/app/src/main/java/edu/northeastern/markergo/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-import edu.northeastern.markergo.tempForNow.Temp;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
