b1c55afb462d96a0e27915574476a5ee208b3c7b
GrishmaAlshi <grishma.alshi@gmail.com>
2023-04-07 13:52:09 -0400

    Location uipdates

--- MarkerGo/app/src/main/java/edu/northeastern/markergo/landingPage.java
+++ MarkerGo/app/src/main/java/edu/northeastern/markergo/landingPage.java
@@ -1,12 +1,16 @@
 package edu.northeastern.markergo;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -41,6 +45,7 @@ public class landingPage extends AppCompatActivity implements OnMapReadyCallback
     public double latitude;
     public double longitude;
     LocationRequest locationRequest;
+//    GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +53,10 @@ public class landingPage extends AppCompatActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_landing_page);
 
         locationRequest = LocationRequest.create();
-
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
         drawerLayout = findViewById(R.id.my_drawer_layout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
 
@@ -77,23 +82,49 @@ public class landingPage extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if (location != null) {
-            latitude = location.getLatitude();
-            longitude = location.getLongitude();
-        } else {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            System.out.println("NOPERMISSION");
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        System.out.println(latitude+" " +longitude);
-        LatLng currentLoc = new LatLng(latitude,longitude);
+        LatLng currentLoc = new LatLng(location.getLatitude(), location.getLongitude());
         googleMap.addMarker(new MarkerOptions()
                 .position(currentLoc)
                 .title("Current Location")
                 .snippet("Tag: Random")
         );
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLoc,5));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLoc, 5));
     }
 
 
-
+//    @Override
+//    public void onLocationChanged(@NonNull Location location) {
+//        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+////        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//
+//        LatLng currentLoc = new LatLng(location.getLatitude(), location.getLongitude());
+//        googleMap.addMarker(new MarkerOptions()
+//                .position(currentLoc)
+//                .title("Current Location")
+//                .snippet("Tag: Random")
+//        );
+//        this.googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLoc,5));
+//    }
 }
