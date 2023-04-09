8dabf41324759de736d23d139872652ad713460d
tanmaykapoor <tanmay.skater@gmail.com>
2023-04-09 17:53:59 -0400

    add change profile photo option

--- MarkerGo/app/src/main/java/edu/northeastern/markergo/UserProfileActivity.java
+++ MarkerGo/app/src/main/java/edu/northeastern/markergo/UserProfileActivity.java
@@ -1,20 +1,33 @@
 package edu.northeastern.markergo;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.facebook.AccessToken;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
@@ -25,6 +38,9 @@ public class UserProfileActivity extends AppCompatActivity {
     private TextView usernameTV;
     private ImageView userDP;
     private TextView email;
+    private static int PICK_IMAGE_REQUEST_CODE = 1;
+    private StorageReference storageRef;
+    private StorageReference imagesRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +48,8 @@ public class UserProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_profile);
 
         mAuth = FirebaseAuth.getInstance();
+        storageRef = FirebaseStorage.getInstance().getReference();
+        imagesRef = storageRef.child("customUserPhotos/");
 
         usernameTV = findViewById(R.id.username);
         userDP = findViewById(R.id.userDP);
@@ -66,6 +84,81 @@ public class UserProfileActivity extends AppCompatActivity {
             Thread setUserDP = new Thread(new UrlToBitmap(photoUrl));
             setUserDP.start();
         }
+    }
+
+    public void updateUserDP(View view) {
+        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        getIntent.setType("image/*");
+
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/*");
+
+        Intent captureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+
+        Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent, captureIntent});
+
+        startActivityForResult(chooserIntent, PICK_IMAGE_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            Toast.makeText(this, "Image picked", Toast.LENGTH_SHORT).show();
+
+            Uri file;
+            if (data.getData() != null) {
+                file = data.getData();
+            } else {
+                Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+                String path = MediaStore.Images.Media.insertImage(getApplicationContext().getContentResolver(), imageBitmap, "Title", null);
+                file = Uri.parse(path);
+            }
+
+            // call only if matches marker location details?
+            uploadPhotoToDb(file);
+        }
+    }
+
+    private void uploadPhotoToDb(Uri file) {
+        StorageReference ref = imagesRef.child(file.getLastPathSegment());
+        UploadTask uploadTask = ref.putFile(file);
+        uploadTask.addOnSuccessListener(taskSnapshot -> {
+                    Log.i("status", "upload successful");
+                })
+                .addOnFailureListener(e -> {
+                    Log.i("status", "upload failed");
+                });
+        uploadTask.continueWithTask(task -> {
+                    if (task.isSuccessful()) {
+                        return ref.getDownloadUrl();
+                    } else {
+                        throw Objects.requireNonNull(task.getException());
+                    }
+                })
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Uri uri = task.getResult();
+                        updateUserProfile(uri);
+                    } else {
+                        Log.i("status", "failed to get download url");
+                    }
+                });
+    }
+
+    private void updateUserProfile(Uri photoUri) {
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setPhotoUri(photoUri)
+                .build();
+        user.updateProfile(profileUpdates)
+                .addOnSuccessListener(unused -> {
+                    Thread setUserDP = new Thread(new UrlToBitmap(photoUri.toString()));
+                    setUserDP.start();
+                })
+                .addOnFailureListener(e -> Log.i("photoUri", "not updated"));
     }
 
     private class UrlToBitmap implements Runnable {
